d2a6fd12acde548a8b24c2e9193c33fa1ed7d994
junsehyeon <shjun1210@icloud.com>
2025-07-10 16:45:55 +0900

    add ch01

---
+++ ch01/src/sub2/PrintTest.java
@@ -0,0 +1,21 @@
+package sub2;
+
+/*
+ 날짜 : 2025/07/07
+ 이름 : 전세현
+ 내용 : Java 다양한 출력 실습
+  */
+public class PrintTest {
+ public static void main(String[] args) {
+	 // 기본출력
+	 System.out.println("Hello Java!");//한줄복사 ctrl+alt+방향키 아래
+	 System.out.println("Hello Java!");//한줄삭제 ctrl+d
+	 System.out.println("Hello Java!");//작업취소 ctrl+z
+	 // 한줄출력
+	 System.out.println("Welcome"); //ln은 줄바꿈 = 개행
+	 System.out.println("java");
+	 // 개행출력
+	 System.out.print("Welcome\n"); // \n: 개행처리 이스케이프 특수 문자
+	 System.out.print("kOREA");
+ }
+}
